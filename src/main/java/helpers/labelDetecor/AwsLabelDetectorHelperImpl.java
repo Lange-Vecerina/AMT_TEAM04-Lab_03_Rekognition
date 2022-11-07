02a31f139e3a8786bc5a4156332588e884cfec8d
yanik23 <ylange@windowslive.com>
2022-11-07 21:38:07 +0100

    adding labeldetector
    Signed-off-by: yanik23 <ylange@windowslive.com>

--- src/main/java/helpers/labelDetecor/AwsLabelDetectorHelperImpl.java
+++ src/main/java/helpers/labelDetecor/AwsLabelDetectorHelperImpl.java
@@ -1,4 +1,47 @@
 package helpers.labelDetecor;
 
+
+
+
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.services.rekognition.RekognitionClient;
+import software.amazon.awssdk.services.rekognition.model.*;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+
 public class AwsLabelDetectorHelperImpl {
+
+    public static void detectImageLabels(RekognitionClient rekClient, String sourceImage, int nbOfLabels, double minConfidence) {
+
+        try {
+            InputStream sourceStream = new FileInputStream(sourceImage);
+            SdkBytes sourceBytes = SdkBytes.fromInputStream(sourceStream);
+
+            // Create an Image object for the source image.
+            Image souImage = Image.builder()
+                    .bytes(sourceBytes)
+                    .build();
+
+            DetectLabelsRequest detectLabelsRequest = DetectLabelsRequest.builder()
+                    .image(souImage)
+                    .maxLabels(nbOfLabels)
+                    .minConfidence((float)minConfidence)
+                    .build();
+
+            DetectLabelsResponse labelsResponse = rekClient.detectLabels(detectLabelsRequest);
+            List<Label> labels = labelsResponse.labels();
+            System.out.println("Detected labels for the given photo");
+            for (Label label: labels) {
+
+                System.out.println(label.name() + ": " + label.confidence().toString());
+            }
+
+        } catch (RekognitionException | FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+    }
 }
