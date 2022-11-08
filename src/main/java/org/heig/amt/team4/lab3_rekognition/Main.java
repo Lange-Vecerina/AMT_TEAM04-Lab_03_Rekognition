f9304f190fb7b60f67130af4bcee1a3d5338a873
Ivan R. Vecerina <IvanR.vecerina@gmail.com>
2022-11-08 17:23:05 +0100

    Update Main.java

--- src/main/java/org/heig/amt/team4/lab3_rekognition/Main.java
+++ src/main/java/org/heig/amt/team4/lab3_rekognition/Main.java
@@ -6,7 +6,13 @@ import software.amazon.ion.Timestamp;
 public class Main {
     public static void main(String[] args) {
         // take the first argument as the image path
-        String imagePath = args[0];
+        String imagePath;
+
+        if (args.length > 0) {
+            imagePath = args[0];
+        } else {
+            imagePath = "src/main/resources/cars.jpg";
+        }
 
         // Make a string constant of current date and time
         String date = Timestamp.now().toString();
@@ -23,7 +29,7 @@ public class Main {
         System.out.println("Link to the image: " + link);
 
         // Get the labels of the image
-        String labels = client.labelDetector().analyze(args[0], 10, 60f);
+        String labels = client.labelDetector().analyze(imagePath, 10, 60f);
 
         // Put the labels in a new file and upload it to the bucket
         client.dataObjectHelper().create(bucketPath + "labels.txt", labels.getBytes());
